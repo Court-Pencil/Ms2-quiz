0b89ee2a10ad73f847d174105d96540f47278468
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-01 17:53:06 +0000

    added statehook for menu and added conditional rednering to show one component at a time

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,18 @@ import './App.css';
 import Quiz from './components/Questions'
 import EndScreen from './components/EndScreen';
 import "nes.css/css/nes.min.css";
+import { useState, useEffect } from 'react';
+import Menu from './components/Menu';
+import Questions from './components/Questions';
 
 function App() {
+  const [ stage, setStage] = useState("menu");
   return (
-    <div className="App">
-      <Home/>
-      <Quiz/>
-      <EndScreen/>
+    <div>
+      {stage === 'menu' && <Menu />}
+      {stage === 'quiz' && <Questions/>}
+      {stage === 'end' && <EndScreen />}
+
     </div>
   );
 }
