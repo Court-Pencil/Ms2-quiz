7207861346f410c22f8f89456bd9d0904286f360
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 17:30:28 +0000

    started again to include different screen transitions

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import Questions from './components/Questions'
 import './App.css'
 
-export default function App() {
+export default function App() { 
+  const [stage, setStage] = React.useState('start')
   return (
     <div className="App">
-      <Questions />
+      {stage === 'start' && (
+        <div>
+          <h1>Welcome to the Video Game Trivia!</h1>
+          <button onClick={() => setStage('quiz')}>Start Quiz</button>
+        </div>
+      )}
+      {stage === 'quiz' && <Questions />}
+       {stage === 'results' && <div>Your results will be displayed here.</div>}
     </div>
   )
 }
