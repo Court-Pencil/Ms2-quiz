50b7254160c196bd75f12b19ae02b2d8af536fa1
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 13:32:47 +0000

    started to build the quiz component in questions.jsx. added states

--- my-app/src/components/Questions.jsx
+++ my-app/src/components/Questions.jsx
@@ -1,8 +1,24 @@
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Questions() {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] =   useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+
+   const fetchQuizQuestions = async () => {
+      const response = await fetch(`https://opentdb.com/api.php?amount=10&category=15&type=multiple&difficulty=${difficulty}`);
+      const data = await response.json();
+      console.log(data);
+      setQuestions(data.results);
+   };
+    useEffect(() => {
+       fetchQuizQuestions();
+   }, []);  
   return (
+
     <div>
       Questions
     </div>
