7b92f22caf76346916f74688316dace37f478a09
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 18:27:51 +0000

    added loading and error state to keep track of those. plus a function to convert HTML entities to normal characters

--- my-app/src/components/Questions.jsx
+++ my-app/src/components/Questions.jsx
@@ -1,30 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, use} from 'react'
 
 export default function Questions() {
+   const [questions, setQuestions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [showFeedback, setShowFeedback] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+   const decodeHtmlEntities = (text) => {
+      const textArea = document.createElement('textarea');
+      textArea.innerHTML = text;
+      return textArea.value;
+   }
 
    const fetchQuestions = async () => {
    try {
       const response = await fetch(
-         'https://jsonplaceholder.typicode.com/posts?_limit=10https://opentdb.com/api.php?amount=10&category=15&type=multiple'
+         'https://opentdb.com/api.php?amount=10&category=15&type=multiple'
       );
       const data = await response.json();
-      setCurrentQuestion(data);
+       setCurrentQuestion(data.results);
+      console.log(data.results);
+
    } catch (error) {
       console.log(error);
    }
 };
-  return (
+                                                                                                                                                                                                                                                                                                                                             
+   useEffect(() => {
+      fetchQuestions();
+   }, []);
+
+   return (
     <div>
       <h1>Video Game Trivia</h1>
         <div className="question-section">
+          
+      </div>
+
         <button>Next</button>     
 
     </div>
-    </div>
+    
+    
   )
 }
 
