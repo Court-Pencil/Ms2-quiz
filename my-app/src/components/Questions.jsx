f26f013dd4be429e3e361ba6d6fd547f0c12cb63
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 18:02:18 +0000

    added states to Question.jsx and asyn await try catch block for api source

--- my-app/src/components/Questions.jsx
+++ my-app/src/components/Questions.jsx
@@ -1,14 +1,29 @@
 import React, {useState} from 'react'
 
 export default function Questions() {
-    const [questions, setQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [showFeedback, setShowFeedback] = useState(false);
 
+   const fetchQuestions = async () => {
+   try {
+      const response = await fetch(
+         'https://jsonplaceholder.typicode.com/posts?_limit=10https://opentdb.com/api.php?amount=10&category=15&type=multiple'
+      );
+      const data = await response.json();
+      setCurrentQuestion(data);
+   } catch (error) {
+      console.log(error);
+   }
+};
   return (
     <div>
       <h1>Video Game Trivia</h1>
-        
-        <button>Submit Answers</button>     
+        <div className="question-section">
+        <button>Next</button>     
 
+    </div>
     </div>
   )
 }
