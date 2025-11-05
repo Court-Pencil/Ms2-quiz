0e93edf59902a200b80a154eee94909d4862df5c
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 13:35:20 +0000

    set up basic page to work on question component. imported react, question component and css

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,22 +1,11 @@
-import Home from './components/Menu'
-import './App.css';
-import Quiz from './components/Questions'
-import EndScreen from './components/EndScreen';
-import "nes.css/css/nes.min.css";
-import { useState, useEffect } from 'react';
-import Menu from './components/Menu';
-import Questions from './components/Questions';
+import React from 'react'
+import Questions from './components/Questions'
+import './App.css'
 
-function App() {
-  const [ stage, setStage] = useState("menu");
+export default function App() {
   return (
-    <div>
-      {stage === 'menu' && <Menu />}
-      {stage === 'quiz' && <Questions/>}
-      {stage === 'end' && <EndScreen />}
-
+    <div className="App">
+      <Questions />
     </div>
-  );
+  )
 }
-
-export default App;
