7da1dfd7b6b8022509b7c0e9b5333d658659f497
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-05 13:33:29 +0000

    basic menu jsx added will add more when building further

--- my-app/src/components/Menu.jsx
+++ my-app/src/components/Menu.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>
-      Menu
+    <div className="menu">
+      <h1>Video Game Trivia</h1>
+      <span>Test your knowledge about video games!</span>
+      <button className='start-btn'>Start Quiz</button>
     </div>
   )
 }
